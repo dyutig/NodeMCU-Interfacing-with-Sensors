8fbddad2c8f3aeb65d71a956d4ae2746697aa352
dyutig <80844300+dyutig@users.noreply.github.com>
2021-08-20 10:41:01 +0530

    Add files via upload

---
+++ Juggling with LED.ino
@@ -0,0 +1,25 @@
+
+void setup()
+{
+  pinMode(16, OUTPUT);
+pinMode(5, OUTPUT);
+pinMode(4, OUTPUT);
+
+}
+
+
+void loop()
+{
+
+    digitalWrite(16,HIGH);
+    delay(1000);
+    digitalWrite(5,HIGH);
+    delay(1000);
+    digitalWrite(4,HIGH);
+    delay(1000);
+    digitalWrite(4,LOW);
+    digitalWrite(5,LOW);
+    digitalWrite(16,LOW);
+    delay(1000);
+
+}
